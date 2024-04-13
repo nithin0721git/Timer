5743727ff3cf6f5097c7a5b22efacb573806cdb4
nithin0721git <133486273+nithin0721git@users.noreply.github.com>
2024-04-13 19:03:21 +0530

    commit 2

--- timer.py
+++ timer.py
@@ -2,7 +2,8 @@ import streamlit as st
 import datetime
 import time
 
-def countdown_timer(target_date):
+def countdown_timer():
+    target_date = datetime.datetime(2024, 9, 18, 0, 0)  # Set the target date and time
     current_date = datetime.datetime.now()
     time_difference = target_date - current_date
 
@@ -16,23 +17,31 @@ def countdown_timer(target_date):
     months = days // 30
     days = days % 30
 
-    return months, days, hours, minutes, seconds, milliseconds
+    current_date = datetime.datetime.now()
+    time_difference = target_date - current_date
+    total_days = time_difference.days
+    total_seconds = time_difference.total_seconds()
+    total_hours, remainder = divmod(total_seconds, 3600)
+
+    return months, days, hours, minutes, seconds, milliseconds, total_days, total_hours
 
 def main():
     st.markdown(
         "<h1 style='font-weight: bold; font-size: 70px; text-align: center;'>Countdown Timer</h1>",
         unsafe_allow_html=True
     )
-    target_date = datetime.datetime(2024, 9, 18, 0, 0)  # Set the target date and time
+
 
     timer_placeholder = st.empty()
 
     while True:
-        months, days, hours, minutes, seconds, milliseconds = countdown_timer(target_date)
+        months, days, hours, minutes, seconds, milliseconds,total_days, total_hours = countdown_timer()
         timer_placeholder.write(f"<h2 style='font-weight: bold; font-size: 40px; text-align: center;'>Time Remaining: </h2></br></br>"
                                 f"<h2 style='font-weight: bold; font-size: 60px; text-align: center;'>{months} months {days} days</h2>"
-                               f"<h3 style='font-weight: bold; font-size: 50px; text-align: center;'>{hours} hours, {minutes} minutes, {seconds} seconds, {milliseconds} milliseconds</h3>", unsafe_allow_html=True)
-        time.sleep(0.001)  # Wait for 1 millisecond before updating the timer
+                               f"<h3 style='font-weight: bold; font-size: 50px; text-align: center;'>{hours} hours, {minutes} minutes, {seconds} seconds, {milliseconds} milliseconds</h3>"
+                                f"<h3 style='font-weight: bold; font-size: 60px; text-align: center;'>{total_days} Days</h3>"
+                               f"<h3 style='font-weight: bold; font-size: 60px; text-align: center;'>{total_hours} Hours</h3>", unsafe_allow_html=True)
+        time.sleep(0.01)  # Wait for 10 millisecond before updating the timer
 
 if __name__ == "__main__":
     main()
